8dc10037f204ff18794b2db684af99a19b113044
d2x3 <d2x3@outlook.com>
2019-01-21 23:58:09 +0800

    :beers: research golang start with gin

---
+++ routers/api/v1/article.go
@@ -0,0 +1,123 @@
+package v1
+
+import (
+	"github.com/Unknwon/com"
+	"github.com/astaxie/beego/validation"
+	"github.com/dzxs/blog/models"
+	"github.com/dzxs/blog/pkg/e"
+	"github.com/dzxs/blog/pkg/setting"
+	"github.com/dzxs/blog/pkg/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetArticle(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必须大于0")
+
+	code := e.INVALID_PARAMS
+	var data interface{}
+	if ! valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			data = models.GetArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			logging.Info(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": e.GetMsg(code),
+		"data": data,
+	})
+}
+func GetArticles(c *gin.Context) {
+	data := make(map[string]interface{})
+	maps := make(map[string]interface{})
+	valid := validation.Validation{}
+
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] =state
+		valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+	}
+
+	var tagId int = -1
+	if arg := c.Query("tag_id"); arg != "" {
+		tagId = com.StrTo(arg).MustInt()
+		maps["tag_id"] = tagId
+		valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
+	}
+
+	code := e.INVALID_PARAMS
+	if ! valid.HasErrors() {
+		code = e.SUCCESS
+		data["lists"] = models.GetArticles(util.GetPage(c), setting.PageSize, maps)
+		data["total"] = models.GetArticleTotal(maps)
+	} else {
+		for _, err := range valid.Errors {
+			logging.Info(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": e.GetMsg(code),
+		"data": data,
+	})
+}
+
+func AddArticle(c *gin.Context) {
+	tagId := com.StrTo(c.Query("tag_id")).MustInt()
+	title := c.Query("title")
+	desc := c.Query("desc")
+	content := c.Query("content")
+	createdBy := c.Query("created_by")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
+	valid.Required(title, "title").Message("标题不能为空")
+	valid.Required(desc, "desc").Message("简述不能为空")
+	valid.Required(content, "content").Message("内容不能为空")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.Required(state, "state").Message("状态只允许0或1")
+
+	code := e.INVALID_PARAMS
+	if ! valid.HasErrors() {
+		if models.ExistTagByID(tagId) {
+			data := make(map[string]interface{})
+			data["tag_id"] = tagId
+			data["title"] = title
+			data["desc"]= desc
+			data["content"] = content
+			data["created_by"] = createdBy
+			data["state"] = state
+
+			models.AddArticle(data)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			logging.Info(err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": e.GetMsg(code),
+		"data": make(map[string]interface{}),
+	})
+}
+func EditArticle(c *gin.Context) {}
+func DeleteArticle(c *gin.Context) {}
